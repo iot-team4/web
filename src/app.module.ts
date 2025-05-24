1f85b9fa6bea3ed832c3beaec57fd98283e6d2b4
NicoDora <pjh76574734@gmail.com>
2025-05-24 18:43:12 +0900

    setting: prisma module, service 파일 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from '@src/app.controller';
+import { AppService } from '@src/app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'node:path';
+import { PrismaModule } from '@src/prisma/prisma.module';
 
 @Module({
-  imports: [ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..',  'frontend', 'build'),
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'frontend', 'build'),
       exclude: ['/api*'],
-    }),],
+    }),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
