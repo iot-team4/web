e384ab6e2ff11dfc9267349396755eb64befd8bd
NicoDora <pjh76574734@gmail.com>
2025-05-24 18:44:20 +0900

    setting: biome lineWidth 100으로 변경, import 절대경로로 설정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService } from '@src/app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get("api/hello")
+  @Get('api/hello')
   getHello(): string {
     return this.appService.getHello();
   }
