65868ca923369c556276ed5e39dc7767a3df3dc9
NicoDora <pjh76574734@gmail.com>
2025-05-24 17:18:44 +0900

    setting: 폴더구조 개선

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+import { join } from 'node:path';
 
 @Module({
   imports: [ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client'),
+      rootPath: join(__dirname, '..', '..', 'frontend', 'build'),
       exclude: ['/api*'],
     }),],
   controllers: [AppController],
